f797a2d508aa1c0d0ff32ab899bfde07145efe97
dashsubhankar <dashsubhankar@gmail.com>
2016-09-25 21:40:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/articleone', function(req,res)
 });
 app.get('/articletwo', function(req,res)
 {
-   res.send('Article two requested and will be served here') ;
+    res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 app.get('/articlethree', function(req,res)
 {
