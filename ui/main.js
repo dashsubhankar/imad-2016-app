b483210099897efeb7186de24dd4ebf573fc6cc1
dashsubhankar <dashsubhankar@gmail.com>
2016-10-11 18:21:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,10 +4,16 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
-    element.stle.marginLeft=marginLeft + 'px';
+    element.style.marginLeft=marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 element.onclick = function()
 {
     var interval = setInterval(moveRight,100);
     element.innerHTML= 'YOLO SDASH!';
+};
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+    var interval = setInterval(moveRight,100);
 };
