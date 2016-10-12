5a4ff742805c80e2f84a73b926447c9ca8b751ea
dashsubhankar <dashsubhankar@gmail.com>
2016-10-12 16:06:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,3 +33,11 @@ img.onclick = function()
     var interval = setInterval(moveRight,100);
 };
 */
+var counter = 0;
+var button = document.getElementById('counter');
+button.onclick = function()
+{
+  counter++;
+  var span = document.getElementById('count');
+    span.innerHTML= counter.toString();
+};
