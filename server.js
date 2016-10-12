0d49296aae4d6cb36b2e3fe63423bbe08b2a4291
dashsubhankar <dashsubhankar@gmail.com>
2016-10-12 15:30:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,12 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+var counter =0;
+app.get('/counter',function(req,res)
+{
+   counter++;
+   res.send(counter.toString());
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
