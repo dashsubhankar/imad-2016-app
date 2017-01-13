15f37996eb7d20390162c8d60dc549d06a87cbc5
dashsubhankar <dashsubhankar@gmail.com>
2017-01-13 12:48:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
 //var element  = document.getElementById('maintext');
 //var marginLeft=0;
 /*function moveRight()
